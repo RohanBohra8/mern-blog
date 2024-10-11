e54db525ab0f252416df3254249d6187114aa98c
Rohan Bohra <rohanbohra8@gmail.com>
2024-10-11 21:05:41 +0530

    Header Navbar Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,19 @@ import Dashboard from './pages/Dashboard'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/Signin'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/Projects" element={<Projects />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   )
