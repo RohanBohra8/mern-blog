cc973bbdfb174dfe61268da42fdc333b4f6ea318
Rohan Bohra <rohanbohra8@gmail.com>
2025-01-12 11:23:56 +0530

    Minor Fix

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 
 export default function DashProfile() {
     const { currentUser } = useSelector((state) => state.user);
-
     return (
         <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
