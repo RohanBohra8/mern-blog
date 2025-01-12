37d55b9c820c11937e59ca536d5fcb65df59ce27
Rohan Bohra <rohanbohra8@gmail.com>
2025-01-12 18:47:19 +0530

    complete update , delete , signout user api functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -36,9 +36,9 @@ export const updateUser = async (req, res, next) => {
           errorHandler(400, 'Username can only contain letters and numbers')
         );
       }
+      //profilePicture: req.body.profilePicture, //the profile pic updatation  
       try {
         const updatedUser = await User.findByIdAndUpdate(
-        //profilePicture: req.body.profilePicture, //the profile pic updatation  
           req.params.userId,
           {
             $set: {
@@ -47,14 +47,36 @@ export const updateUser = async (req, res, next) => {
               
               password: req.body.password,
             },
-          },
-          { new: true }
-        );
-        const { password, ...rest } = updatedUser._doc;
-        res.status(200).json(rest);
-      } catch (error) {
-        next(error);
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updatedUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
       }
     }
   };
 
+  export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to delete this user'));
+    }
+    try {
+      await User.findByIdAndDelete(req.params.userId);
+      res.status(200).json('User has been deleted');
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  export const signout = (req, res, next) => {
+    try {
+      res
+        .clearCookie('access_token')
+        .status(200)
+        .json('User has been signed out');
+    } catch (error) {
+      next(error);
+    }
+  };
